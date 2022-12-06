1e1d9acb7f074de67ff70ec758c1656980242bc4
Melda-a10 <74878053+Melda-a10@users.noreply.github.com>
2022-12-05 23:33:11 -0400

    Class 1: Dinosaur

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,17 @@
+from pygame.sprite import Sprite
+
+from dino_runner.utils.constants import RUNNING
+
+
+class Dinosaur(Sprite):
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = 80
+        self.dino_rect.y = 310 
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
